746a4390829a5234b733b5b1c049d75f34612c97
Yubeen Lee <yublee@student.42london.com>
2023-11-14 19:22:51 +0000

    almostmandatory2

--- 1114/ft_strrchr.c
+++ 1114/ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: yublee <yublee@student.42london.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/06 18:52:06 by yublee            #+#    #+#             */
-/*   Updated: 2023/11/14 16:37:31 by yublee           ###   ########.fr       */
+/*   Updated: 2023/11/14 19:21:14 by yublee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,8 @@ char	*ft_strrchr(const char *s, int c)
 	{
 		if (*(s + len) == (unsigned char)c)
 			return ((char *)s + len);
-		if(len == 0)
-			break;
+		if (len == 0)
+			break ;
 		len--;
 	}
 	return (NULL);
