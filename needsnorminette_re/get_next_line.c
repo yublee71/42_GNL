2d6c45e84b48f37c52dec8bd7b8e4740144c5609
yublee71 <146713819+yublee71@users.noreply.github.com>
2023-11-30 09:13:24 +0000

    workingwith36lines

---
+++ needsnorminette_re/get_next_line.c
@@ -0,0 +1,64 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yublee <yublee@student.42london.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/21 15:24:09 by yublee            #+#    #+#             */
+/*   Updated: 2023/11/21 22:32:44 by yublee           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*get_line(char *stored)
+{
+	char	*temp;
+	size_t	i;
+
+	i = ft_charcheck(stored, '\n');
+	temp = stored;
+	stored = ft_substr(stored, i, ft_strlen(stored)); //needs to free
+	free(temp);
+	return (stored);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*stored;
+	char		*temp;
+	char		*line;
+	char		*buffer;
+
+	if (!stored)
+		stored = ft_strdup("");
+	if ((fd && fd <= 3) || fd >= 1000 || BUFFER_SIZE < 0 || !stored)
+		return (NULL);
+	if (ft_charcheck(stored, '\n'))
+	{
+		line = ft_substr(stored, 0, ft_charcheck(stored, '\n'));
+		stored = get_line(stored);
+		return (line);
+	}
+	buffer = (char *)calloc(BUFFER_SIZE + 1, 1);
+	if (read(fd, buffer, BUFFER_SIZE))
+	{
+		buffer[BUFFER_SIZE] = '\0';
+		temp = stored;
+		stored = ft_strjoin(stored, buffer); //needs to free
+		free(temp);
+		free(buffer);
+		return (get_next_line(fd));
+	}
+	if (*stored)
+	{
+		line = ft_strdup(stored);
+		free(stored);
+		free(buffer);
+		stored = NULL;
+		return (line);
+	}
+	free(buffer);
+	return (NULL);
+}
